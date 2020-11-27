254af6820e402be4392ab7680951c220a893a2c4
Ryan <ryanccordeiro@gmail.com>
2020-11-26 22:34:56 -0300

    Die implemented

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -3,15 +3,21 @@ const RIGHT = 39
 const UP = 38
 const DOWN = 40
 
-const snakespeed = 50
 const height = 20
 const width = 20
+
+let snakedelay = 40
+let alterdelay = () =>{
+    snakedelay = document.getElementById('delay').value
+}
 
 let interval
 let running = true
 
 const canvas = document.getElementById('screen')
 const context = canvas.getContext('2d')
+let placar = document.querySelector('p#placar')
+
 
 class Screen {
     constructor(height=10,width=10){
@@ -44,52 +50,62 @@ class Snake {
         let LastSnakePart = this.body[this.body.length -1]
         var SnakePart = {x:LastSnakePart.x,y:LastSnakePart.y}
 
-        this.points++
         this.body.push(SnakePart)
+        this.points++
+        console.log(this.points)
+        placar.innerHTML = this.points
+    }
+
+    Movement(axysx,axysy){
+        for (let peer of this.body){
+            if (peer.x === axysx && peer.y === axysy){
+                alert(`Você Perdeu! Mas fez ${this.points} pontos ☺`)
+                clearInterval(interval)
+                document.location.reload(true)
+                break;
+            }
+
+        }
+        this.body.unshift({x: axysx, y : axysy})
+        this.body.pop()
     }
 
     right(){
         if(this.body[0].x == screen.width - 1){
-            this.body.unshift({x: 0, y : this.body[0].y})
-            this.body.pop()
+            this.Movement(0,this.body[0].y)
 
         } else {
-            this.body.unshift({x: this.body[0].x + 1,
-                                    y:this.body[0].y})
-            this.body.pop()
+            this.Movement(this.body[0].x + 1, this.body[0].y)
         }
     }
 
     left(){
         if(this.body[0].x == 0){
-            this.body.unshift({x: screen.width - 1, y : this.body[0].y})
-            this.body.pop()
+            this.Movement(screen.width - 1,this.body[0].y)
 
         } else {
-            this.body.unshift({x: this.body[0].x - 1,
-                y:this.body[0].y})
-            this.body.pop()
+            this.Movement(this.body[0].x - 1, this.body[0].y)
         }
     }
     
     up(){
         if(this.body[0].y == 0){
-            this.body.unshift({x: this.body[0].x, y : screen.height - 1})
-            this.body.pop()
+            this.Movement(this.body[0].x,screen.height - 1)
+            
             
         } else {
-            this.body.unshift({x: this.body[0].x, y : this.body[0].y - 1})
-            this.body.pop()
+            this.Movement(this.body[0].x,this.body[0].y - 1)
+            
         }
     }
 
     down(){
         if (this.body[0].y == screen.height - 1) {
-            this.body.unshift({x: this.body[0].x, y : 0})
-            this.body.pop()
+            this.Movement(this.body[0].x,0)
+            
         } else {
-            this.body.unshift({x: this.body[0].x, y : this.body[0].y + 1})
-            this.body.pop()
+            this.Movement(this.body[0].x,this.body[0].y + 1)
+            
         }
     }
 }
@@ -114,19 +130,19 @@ function keyboard(event) {
     
     clearInterval(interval)
     if (key == UP){
-        interval = setInterval(() => {snake.up()},snakespeed)
+        interval = setInterval(() => {snake.up()},snakedelay)
 
     } else if (key == DOWN) {
        
-        interval = setInterval(() => {snake.down()},snakespeed)
+        interval = setInterval(() => {snake.down()},snakedelay)
 
     } else if(key == RIGHT){
        
-        interval = setInterval(() => {snake.right()},snakespeed)
+        interval = setInterval(() => {snake.right()},snakedelay)
 
     } else if (key==LEFT) {
        
-        interval = setInterval(() => {snake.left()},snakespeed)
+        interval = setInterval(() => {snake.left()},snakedelay)
 
     }
 }
@@ -147,16 +163,6 @@ function Render() {
         snake.AddPoint()
     }
 
-    for (let i in snake.body){
-        let part = snake.body[i]
-        if (snake.body[0].x === part.x && snake.body[0].y === part.y && snake.body.length > 2){
-            //console.log(snake.body.length)
-            //let ok = prompt('Você Perdeu!')
-            //document.location.reload
-        }
-
-    }
-
     requestAnimationFrame(Render)
 
 }
@@ -167,11 +173,5 @@ const fruit = new Fruit()
 
 canvas.setAttribute('width',screen.width)
 canvas.setAttribute('height',screen.height)
-
-
-
-if (!running){
-    
-}
 
 Render()
